341fbe6c29ed1a647453495bb53f1575048e8985
Christoph Weiler <mail@chrisweiler.at>
2021-12-03 11:07:41 +0100

    Refactored for better readability

--- aoc3.js
+++ aoc3.js
@@ -18,8 +18,8 @@ function part1() {
 }
 
 function part2() {
-    const oxygenGeneratorRating = parseInt(iterateAndFilterInput((arr1, arr2) => arr1.length > arr2.length), 2);
-    const co2ScrubberRating = parseInt(iterateAndFilterInput((arr1, arr2) => arr1.length <= arr2.length), 2);
+    const oxygenGeneratorRating = parseInt(iterateAndFilterInput((nrZeroBits, nrOneBits) => nrZeroBits > nrOneBits), 2);
+    const co2ScrubberRating = parseInt(iterateAndFilterInput((nrZeroBits, nrOneBits) => nrZeroBits <= nrOneBits), 2);
     console.log(oxygenGeneratorRating * co2ScrubberRating);
 }
 
@@ -31,8 +31,10 @@ function iterateAndFilterInput(predicate) {
         }
 
         let bits = iterInput.map(x => x[bitIdx]);
+        let nrZeroBits = bits.filter(bit => bit === '0').length;
+        let nrOneBits = bits.filter(bit => bit === '1').length;
         // Finds most/least common value in array according to predicate
-        const filterBit = predicate(bits.filter(bit => bit === '0'), bits.filter(bit => bit === '1')) ? '0' : '1';
+        const filterBit = predicate(nrZeroBits, nrOneBits) ? '0' : '1';
         iterInput = iterInput.filter(x => x[bitIdx] === filterBit);
     }
     return iterInput[0];
