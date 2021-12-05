fcc1e662e65e35168e47420f60a036a919a446ed
Christoph Weiler <mail@chrisweiler.at>
2021-12-05 08:14:35 +0100

    Day4 refactorings

--- aoc4.js
+++ aoc4.js
@@ -10,14 +10,9 @@ for (let i = 2; i + 5 < input.length; i += 6) {
     const nextBoard = input.slice(i, i + 5)
         .map(row => row.split(/\s+/)
             .map(elem => {
-                    return {
-                        marked: false,
-                        value: elem
-                    }
-                }
-            )
+                return {marked: false, value: elem}
+            })
         );
-
     boards.push(nextBoard);
 }
 
@@ -34,7 +29,7 @@ function part1() {
 function part2() {
     bingoGame((board, draw) => {
         boardsToLookAt = boardsToLookAt.filter(b => b != board);
-        if(boardsToLookAt.length === 0) {
+        if (boardsToLookAt.length === 0) {
             calculateResult(board, draw);
             return true;
         }
@@ -43,14 +38,14 @@ function part2() {
 
 function bingoGame(onBingoFn) {
     boardsToLookAt = boards;
-    for(let draw of draws) {
-        for(let board of boardsToLookAt) {
+    for (let draw of draws) {
+        for (let board of boardsToLookAt) {
             const [x, y] = findOnBoard(board, draw);
-            if(x === -1) {
+            if (x === -1) {
                 continue;
             }
             board[y][x].marked = true;
-            if(isBingo(board, x, y) && onBingoFn(board, draw)) {
+            if (isBingo(board, x, y) && onBingoFn(board, draw)) {
                 return;
             }
         }
@@ -62,43 +57,22 @@ function calculateResult(board, draw) {
         .flatMap(x => x)
         .filter(x => !x.marked)
         .map(x => +x.value)
-        .reduce(( accumulator, currentValue ) => accumulator + currentValue, 0);
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
     console.log(nonMarkedSum * (+draw));
 }
 
 function findOnBoard(board, num) {
-    for(let y = 0; y < 5; y++) {
-        for(let x = 0; x < 5; x++) {
-            if(board[y][x].value === num) {
-                return [x,y];
+    for (let y = 0; y < 5; y++) {
+        for (let x = 0; x < 5; x++) {
+            if (board[y][x].value === num) {
+                return [x, y];
             }
         }
     }
     return [-1, -1];
 }
 
+// Look if every elem in the row or column is marked
 function isBingo(board, x, y) {
-    // Look if every elem in the row or column is marked
-    if(board[y].every(elem => elem.marked) || board.map(row => row[x]).every(elem => elem.marked)) {
-        return true;
-    }
-
-    if(x == y) { // if we are on the main diagonals
-        // Look if every elem in the diagonal is marked
-        let bingo = true;
-        for(let i = 0; i < 5; i++) {
-            if(!board[i][i].marked) {
-                bingo = false;
-            }
-        }
-        for(let i = 0, j = 4; i < 5; i++, j--) {
-            if(!board[j][i].marked) {
-                bingo = false;
-            }
-        }
-        if(bingo) {
-            return true;
-        }
-    }
-    return false;
+    return board[y].every(elem => elem.marked) || board.map(row => row[x]).every(elem => elem.marked);
 }
